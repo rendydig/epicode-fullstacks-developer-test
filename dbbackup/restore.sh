113d185d7bef05a70f0afb4fe5b220ebf48e4479
The Code <rendy@upscalix.com.au>
2024-12-21 13:30:37 +0700

    feat: create our docker new entrypoint

--- dbbackup/restore.sh
+++ dbbackup/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wait for PostgreSQL to be ready
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "localhost" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   echo "PostgreSQL is unavailable - sleeping"
   sleep 1
 done
@@ -9,4 +9,4 @@ done
 echo "PostgreSQL is up - executing restore"
 
 # Restore the database
-PGPASSWORD=$POSTGRES_PASSWORD pg_restore -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -v /docker-entrypoint-initdb.d/directus_backup.dump
+PGPASSWORD=$POSTGRES_PASSWORD pg_restore -h "localhost" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -v --clean --if-exists /docker-entrypoint-initdb.d/directus_backup.dump
